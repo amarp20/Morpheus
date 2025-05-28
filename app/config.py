1b1e415c9186974d8739f1a2cee93c93ca3d99d7
amarp20 <knigth.moro@gmail.com>
2025-05-28 09:59:04 +0200

    securizado y mando sin desocupar

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    # Pilla de .env en producción si lo deseas
+
     MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017/Morpheus')
     UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
     ALLOWED_EXTENSIONS = {"xls", "xlsx", "csv"}
